6fa1da635303bfb2dd6302e25bf8ef895af47b55
hebun3421 <hebunyildirim618@gmail.com>
2025-04-07 15:32:47 +0300

    Update start.py

--- ArchMusic/plugins/bot/start.py
+++ ArchMusic/plugins/bot/start.py
@@ -14,7 +14,7 @@ from pyrogram import filters
 from pyrogram.enums import ChatType, ParseMode
 from pyrogram.types import (InlineKeyboardButton,
                             InlineKeyboardMarkup, Message)
-from py_yt import VideosSearch
+youtubesearchpython.__future__ import VideosSearch
 
 import config
 from config import BANNED_USERS
